ee062c4322b3a92778b56450eefe8c4d2bc51d1d
Natalie Chow <cychownat@gmail.com>
2016-01-25 07:58:27 -0800

    Changed default color back to gray

--- models/koala.js
+++ models/koala.js
@@ -4,7 +4,7 @@ var koalaSchema = new mongoose.Schema({
   name: String,
   accent: {type: String, default: 'Australian'},
   age: {type: Number, default: Math.floor(Math.random() * 100)},
-  color: {type: String, default: 'brown'},
+  color: {type: String, default: 'gray'},
   friends: {type: Array, default: []}
 });
 
